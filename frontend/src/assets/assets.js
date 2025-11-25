356e4bffd71ce159245b4f3a705c99b538643460
Ankith Kumar Sara <ankithkumar.sara_2026@woxsen.edu.in>
2025-11-25 17:50:21 +0530

    latest commit

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,9 +1,6 @@
-import p_img1 from './p_img1.jpg'
-
 import logo from './logo.png'
 import logo_white from './logo_white.png'
 import cart_icon from './cart_icon.png'
-import search_icon from './search_icon.png'
 import about_img from './about_img.jpg'
 import contact_img from './contact_img.jpg'
 import razorpay_logo from './razorpay_logo.png'
@@ -14,25 +11,9 @@ export const assets = {
     logo,
     logo_white,
     cart_icon,
-    search_icon,
     about_img,
     contact_img,
     razorpay_logo,
     cross_icon,
     mallesh_img,
 }
-
-export const products = [
-    {
-        _id: "aaaaa",
-        name: "Women Deep Neck Cotton Top",
-        description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
-        price: 1599,
-        image: [p_img1],
-        category: "Women",
-        subCategory: "Topwear",
-        sizes: ["S", "M", "L"],
-        date: 1716634345448,
-        bestseller: true
-    },
-]
